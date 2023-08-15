0e008deca5fd42a308b8062c9e5248c5769f3e95
Leka <alex@talentdatalabs.com>
2023-08-16 00:48:14 +0200

    Location page/README file

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
-## Steps to reproduce
+## Table of Contents
 
-./vendor/bin/sail up
-./vendor/bin/sail artisan migrate
-./vendor/bin/sail artisan db:seed
-./vendor/bin/sail artisan calculate:scores
+1. [General Info](#general-info)
+2. [Technologies](#technologies)
+3. [Installation](#installation)
+
+### General Info
+
+---
+
+Reputation test project made with PHP (Laravel) and Bootstrap 5 for frontend.
+I connected to google maps reviews scraper, exported the data in the form of a CSV file for technomania (electronic equipment store) which I later used in my database. The project has a landing page where you enter the domain (in this case https://www.tehnomanija.rs/) after which you get to the dashboard page where you can see the best and worst scores of all locations. The addresses of the locations are in the form of a link, and by clicking on it, you can access a single location where you can see basic data and a chart.
+
+## Technologies
+
+---
+
+A list of technologies used within the project:
+
+-   [Laravel](https://laravel.com/): Version 8.75
+-   [Bootstrap](https://getbootstrap.com/): Version 5.0.2
+-   [Fontawesome](https://fontawesome.com/): Version 6.4.2
+-   [Apexcharts](https://apexcharts.com/): Version 3.41
+
+## Installation
+
+---
+
+A little intro about the installation.
+
+```
+$ git clone https://github.com/leka91/reputation-project.git
+$ cd reputation-project
+$ composer install
+$ Create a copy of .env file (cp .env.example .env)
+$ php artisan key:generate - create app encryption key
+$ Create an empty database for project using the database tools you prefer
+$ In the .env file fill in the DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, and DB_PASSWORD options to match the credentials of the database
+$ ./vendor/bin/sail up - to start local development server
+$ ./vendor/bin/sail artisan migrate - to migrate database tables
+$ ./vendor/bin/sail artisan db:seed - to fill tables with data
+$ ./vendor/bin/sail artisan calculate:scores - script to calculate reputation scores
+
+```
--- resources/views/pages/location.blade.php
+++ resources/views/pages/location.blade.php
@@ -8,61 +8,106 @@
     Back
 </a>
 
-<h2 class="mb-5">What parameters impact the Reputation Score?</h2>
+<h2>What parameters impact the Reputation Score?</h2>
+
+<p class="mb-5">
+    The overall score for location 
+    <span class="text-primary">{{ $location['address'] }}</span> 
+    is <span style="color: {{ $location['color'] }}; font-weight: bold; font-size: 20px;">{{ $location['total_score'] }}</span> 
+</p>
 
 <div class="row">
     <div class="col">
         <div class="d-flex justify-content-center">
-            <canvas id="myChart1" style="width:100%;max-width:600px"></canvas>
-        </div>
-    </div>
-    <div class="col">
-        <div class="d-flex justify-content-center">
-            <canvas id="myChart2" style="width:100%;max-width:600px"></canvas>
+            <div id="chart" style="width:100%;max-width:600px"></div>
         </div>
     </div>
 </div>
 
-
-
-
-
 @endsection
 
 @section('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
     let reviewsRating = {{ $location['reviews_rating'] }};
     let totalReviews = {{ $location['total_reviews'] }};
     let lastMonthReviews = {{ $location['last_month_reviews'] }};
-//`Last Month Reviews ${lastMonthReviews}`
-    let xValues1 = [`Reviews Rating ${reviewsRating}`];
-    let xValues2 = [`Reviews Number ${totalReviews}`];
-//{{ $location['last_month_reviews_percentile'] }}
-    let yValues1 = [{{ $location['reviews_rating_percentile'] }}];
-    let yValues2 = [{{ $location['total_reviews_percentile'] }}];
-    let barColors = ["red"];
-    
-    new Chart("myChart1", {
-        type: "bar",
-        data: {
-            labels: xValues1,
-            datasets: [{
-                backgroundColor: barColors,
-                data: yValues1
-            }]
-        }
-    });
 
-    new Chart("myChart2", {
-        type: "bar",
-        data: {
-            labels: xValues2,
-            datasets: [{
-                backgroundColor: barColors,
-                data: yValues2
-            }]
+    let reviewsRatingPercentile = {{ $location['reviews_rating_percentile'] }};
+    let totalReviewsPercentile = {{ $location['total_reviews_percentile'] }};
+    let lastMonthReviewsPercentile = {{ $location['last_month_reviews_percentile'] }};
+
+    let percentiles = [reviewsRatingPercentile, totalReviewsPercentile, lastMonthReviewsPercentile];
+
+    let data = [];
+
+    for (let percentile of percentiles) {
+        if (percentile <= 30) {
+            data.push({
+                "color": '#c20606',
+                "label": 'Worst'
+            });
+        } else if (percentile <= 70) {
+            data.push({
+                "color": '#ebd300',
+                "label": 'Average'
+            });
+        } else {
+            data.push({
+                "color": '#42f5bf',
+                "label": 'Good'
+            });
         }
-    });
+
+    }
+
+    let reviewRatingColor = data[0].color;
+    let reviewRatingLabel = data[0].label;
+
+    let totalReviewsColor = data[1].color;
+    let totalReviewsLabel = data[1].label;
+
+    let lastMonthReviewsColor = data[2].color;
+    let lastMonthReviewsLabel = data[2].label;
+
+    let options = {
+        series: [{
+            name: 'Percentile',
+            data: [reviewsRatingPercentile, totalReviewsPercentile, lastMonthReviewsPercentile]
+        }],
+        chart: {
+            height: 350,
+            type: 'bar'
+        },
+        colors: [reviewRatingColor, totalReviewsColor, lastMonthReviewsColor],
+        plotOptions: {
+            bar: {
+                columnWidth: '60%',
+                distributed: true,
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        legend: {
+            show: false
+        },
+        xaxis: {
+            categories: [
+                ['Reviews rating', `${reviewsRating} ${reviewRatingLabel}`],
+                ['Total reviews', `${totalReviews} ${totalReviewsLabel}`],
+                ['Last month reviews', `${lastMonthReviews} ${lastMonthReviewsLabel}`]
+            ],
+            labels: {
+                style: {
+                    colors: [reviewRatingColor, totalReviewsColor, lastMonthReviewsColor],
+                    fontSize: '14px'
+                }
+            }
+        }
+    };
+
+    let chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
 </script>
 @endsection
